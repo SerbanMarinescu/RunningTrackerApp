047dc8f3b0dee95a3a71a8dc816812589d69bd4c
Serban Marinescu <serban.2000@yahoo.com>
2024-06-28 13:48:51 +0300

    Project Wide Utility

--- run/location/build.gradle.kts
+++ run/location/build.gradle.kts
@@ -10,12 +10,6 @@ dependencies {
 
     implementation(libs.androidx.core.ktx)
 
-    implementation(libs.androidx.compose.ui)
-    implementation(libs.androidx.compose.ui.tooling.preview)
-    implementation(libs.androidx.compose.ui.graphics)
-    implementation(libs.androidx.compose.material3)
-    debugImplementation(libs.androidx.compose.ui.tooling)
-
     implementation(libs.kotlinx.coroutines.core)
     implementation(libs.google.android.gms.play.services.location)
 
