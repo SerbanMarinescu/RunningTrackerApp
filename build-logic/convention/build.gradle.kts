bedb02b7f026ce52af72be0de0ee7fecf96f3a17
Serban Marinescu <serban.2000@yahoo.com>
2024-08-02 12:59:54 +0300

    Testing Framework Setup

--- build-logic/convention/build.gradle.kts
+++ build-logic/convention/build.gradle.kts
@@ -54,5 +54,13 @@ gradlePlugin {
             id = "runique.android.application.wear.compose"
             implementationClass = "AndroidApplicationWearComposeConventionPlugin"
         }
+        register("jvmJunit5") {
+            id = "runique.jvm.junit5"
+            implementationClass = "JvmJUnit5ConventionPlugin"
+        }
+        register("androidJunit5") {
+            id = "runique.android.junit5"
+            implementationClass = "AndroidJUnit5ConventionPlugin"
+        }
     }
 }
